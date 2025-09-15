bf00104d01f30903fbcd037ba9c88aa5fdf78671
Rikuto <mikadorikuto@gmail.com>
2025-09-15 15:49:47 +0900

    add some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="min-h-screen bg-gray-50">
       <Header />
-      <main>
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
+      <Footer />
     </div>
   );
 }
